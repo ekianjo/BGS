fe6238a376f9c2c34e32129aba7624eaf03bd897
ekianjo <ekianjo@gmail.com>
2013-04-25 11:24:40 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+# Look for directories (emulators, known native games). List them in array.
+# Go through each directory, list all files and their path. Apply filters if necessary (exceptions). 
+# Make a tar archive or zip. 
+# Complete!
