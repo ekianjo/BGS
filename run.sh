1d0ab8f9fa59f3ab816fa0600e5527ee071c915b
ekianjo <ekianjo@gmail.com>
2013-04-25 13:27:57 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,12 @@
 # Go through each directory, list all files and their path. Apply filters if necessary (exceptions). 
 # Make a tar archive or zip. 
 # Complete!
+
+#find / -name 'httpdocs' -type d
+#the first parameter "/" is where to look, in this case "/" it's the entire system.
+
+#-name could be -iname to ignore case
+
+#also -type is not mandatory
+
+#use : man find for more options
