8a24fcff758782d95ac7a5c62219153d2d500af2
ekianjo <ekianjo@gmail.com>
2013-05-20 15:10:49 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+
+#make a model for each save system : i.e. 
+# Location (folder), filetype (extension), system (machine), game (name)
 # Look for directories (emulators, known native games). List them in array.
 # Go through each directory, list all files and their path. Apply filters if necessary (exceptions). 
 # Make a tar archive or zip. 
