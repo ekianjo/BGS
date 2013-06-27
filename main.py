ad1faf3d4a0e29da072cb1f76ae3059caaf7edd1
ekianjo <ekianjo@gmail.com>
2013-06-27 16:21:57 +0900

    Create main.py
    initial ideas.

---
+++ main.py
@@ -0,0 +1,13 @@
+#need to define templates for save files (top)
+#list dirs in media
+#test for directories such as Pandora -> to accelerate the loops
+#os.path.isdir -> then store in config.
+#allow to refresh directories lookup in one click.
+#display what systems/games are recognized
+#test for directory for emulators and all / and know where they save their saved games – for the ones where path is fixed.
+#for the ones where individual folders need to add option to add appropriate folders and save them in config file. 
+#list all files to be saved, indicate total uncompressed size in status bar as well as number of save files.
+#propose saving compressed data to media (both SD cards) / Cloud / ftp
+#compress files
+#copy files in the right place
+#when executed, register when the last backup was made, as well as the list of what was backed up.
