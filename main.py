f85b8aa91a9d91ae17aa4492a77baee4a10ee55d
ekianjo <ekianjo@gmail.com>
2013-08-02 10:39:57 +0900

    Update main.py
    Ongoing definition of the program structure.

--- main.py
+++ main.py
@@ -4,25 +4,67 @@
 import os
 
 #get media list in /media and record them in variable
-topdirlist=os.listdir("/media")
 
 #check if they are still here at launch of the application
 
-#template should contain following info: name of pnd, name of appdata folder, file extension of save file
+#template should contain following info: name of pnd, name of appdata folder, folder with save files, file extension of save file
 
-progtodetect=["Gambatte.pnd","pcsx.pnd"]
+progtodetect=[("Gambatte.pnd","gambatte-qt",'folder=sav','folder=savestate')]
+appsfolder=['menu','desktop','apps']
 
+def checkprogram(progname,directory):
+  
+  global appsfolder
+  found=0
+  
+  for folder in appsfolder:
+    if os.path.isfile("/media/{0}/pandora/{1}/{2}".format(directory,folder,progname))==True :
+      print "Found "+ progname
+      found+=1
+  if found==0:
+    return False
+  else:
+    return True
+
+def checkappdata(appdatafolder,directory):
+  if os.path.isdir("/media/{0}/pandora/appdata/{1]".format(directory,appdatafolder))==True:
+    return True
+  else:
+    return False
+
+def confirmfolderorfile(element):
+  if "folder=" in element:
+    return True
+  else:
+    return False  
+
+def bgs():
 #check for Pandora folder
-for directory in topdirlist:
-  if os.path.isdir("media/{0}/pandora".format(directory)):
-      pass
+  topdirlist=os.listdir("/media")
+  for directory in topdirlist:
+    print "Checking the SD Card "+directory
+    if os.path.isdir("/media/{0}/pandora".format(directory)):
+      print "Checking for pandora folder on "+directory
+      for progs in progtodetect:
+        print "Checking for "+progs[0]
+        if checkprogram(progs[0])==True:
+          print "Checking for appfolder for "+progs[0]
+          if checkappdata(progs[1])==True:
+            print "Found the appdata folder "+progs[1]
+            
+            for i=2 to len(progs):
+              if confirmfolderorfile(progs[i])==True:
+                #confirm folder is not empty
+                #if not empty save it in list of what to save later
+              else:
+                #find the files with the extension
+                #save their full path location for later saving
+          
+    # give message regarding how many files are being saved, and how big they are uncompressed
+    #save first zip for first SD CARD - add date in name
+    #confirm size after compression
 
 
-  #check in target directories : menu, apps
-
-  #check for PNDs for target applications. 
-  #stores list of PNDs supported found
-  #check for save game definitions / follow templates for PNDs supported, go through the list
   #return a list with media as fist entry, folder as second entry, list of files detected as third entry
   
   
