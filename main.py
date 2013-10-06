94030dc21921ad35679c9f87709b5963a9b8aa73
ekianjo <ekianjo@gmail.com>
2013-10-06 12:18:13 +0900

    Update main.py
    Many changes done offline and inserted back here at once. Full functional version on Pandora.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #need to define templates for save files (top)
 #list dirs in media
 
-import os, argparse,tarfile, time
+import os, tarfile, time, shutil
+import argparse, PyZenity, glob
 from datetime import date
 
 #will do argparse later
-parser = argparse.ArgumentParser(description='This Program Backups Game Saves for Major Emulators on the Open Pandora Handheld')
+#parser = argparse.ArgumentParser(description='This Program Backups Game Saves for Major Emulators on the Open Pandora Handheld')
 # -g / start GUI
 # -h / help
 # -b / which card to backup (sd card 1 or 2 or all)
@@ -16,9 +17,40 @@ today=date.today()
 #check if they are still here at launch of the application
 #template should contain following info: name of pnd, name of appdata folder, folder with save files, file extension of save file
 
-listoftemplates=[("Gambatte.pnd","gambatte-qt",['sav','savestate'],[],[])]
+listoftemplates=[
+("gambatte.pnd","gambatte-qt",['saves','.config'],[],[])
+,('pcsx_rearmed_r19.pnd','pcsx_rearmed',['.pcsx','screenshots'],[],[])
+,('drastic.pnd','DraStic',['.links','backup','config','profiles','savestates'],[],[])
+,('PPSSPP.pnd','PPSSPP',['home'],[],[])
+,('gpSP-pnd-0.9-2Xb-u8a.pnd','gpsp',['',''],[],[])
+,('UAE4ALLv2.0.pnd','uae4all',['saves','conf'],[],[])
+,('snes9x4p_20120226.pnd','snes9x.skeezix.alpha',['.snes96_snapshots'],[],[])
+,('gpfce_r2.pnd','gpfce',['fceultra'],[],[])
+,('gngeo_0.8.5.pnd','gngeo_pepone',['save','conf'],[],[])
+,('fba.pnd','fba-dave18',['savestates','screenshots','conf','config'],[],[])
+,('apkenv-v42.3.17.2.pnd','apkenv',['data'],[],[])
+,('darkplaces.pnd','darkplaces',['.darkplaces'],[],[])
+,('dunedynasty.pnd','dunedynasty',['dunedynasty'],[],[])
+,('scummvm-op.pnd','scummvm',['saves'],[],[])
+,('rtcw.pnd','rtcw',['home'],[],[])
+,('PicoDrive_190.pnd','picodrive',['srm','mds','cfg','brm'],[],[])
+,('freespace2.pnd','freespace2',['home'],[],[])
+,('exult-1.4.pnd','exult',['.exult'],[],[])
+,('cdoom.pnd','doom',['.chocolate-doom'],[],[])
+,('solarusdx-zsdx-11201.pnd','solarusdx',['.solarus'],[],[])
+,('pewpew2.pnd','pewpew2',['documents'],[],[])
+,('duke3d.pnd','duke3d',['.eduke32'],[],[])
+,('8Blitter.pnd','8blitter.lordus',['data'],[],[])
+,('pushover.pnd','pushover',['.pushover'],[],[])
+,('projectx_ptitseb.pnd','projectx',['savegame'],[],[])
+#,('','',['',''],[],[])
+#,('','',['',''],[],[])
+#,('','',['',''],[],[])
+]
+
 appsfolder=['menu','desktop','apps']
 directorytobackup=[]
+programsfound=[]
 
 directories=""
 debug=True
@@ -26,66 +58,69 @@ debug=True
 #function that will do the backup itself
 def backupspecific(progname,appdatafolder,listfolders,listfiles):
   
-  global directories, directorytobackup
+  global directories, directorytobackup, programsfound
   
   for topdirectory in directories:
-    if checkprogram(progname,topdirectory)!="":
-      workingfolder=checkprogram(progname)
-      print workingfolder
-      #confirm the top level working folder under /media/xx/pandora/PAF  - it is not used after expect for path
+	  
+	    workingfolder=checkprogram(progname,topdirectory)
+	    if workingfolder!="":
+		  programsfound.append(progname)
+		  
+		  print "working folder is "+ workingfolder
+		  #confirm the top level working folder under /media/xx/pandora/PAF  - it is not used after expect for path
+	  
+		  if checkappdata(appdatafolder,topdirectory)==False:
+			print "the program " + progname +".pnd does not have a appdata folder so far." 
+		  else:
+			
+			directoriesinappfolder=os.listdir(("/media/{0}/pandora/appdata/{1}").format(topdirectory,appdatafolder))
+			
+			#marks folders to save
+			for onefolder in directoriesinappfolder:
+			  for foldertobackup in listfolders:
+				if onefolder==foldertobackup:
+				  print progname+":found folder "+ onefolder+" to backup"				  
+				  directorytobackup.append(("/media/{0}/pandora/appdata/{1}/{2}").format(topdirectory,appdatafolder,onefolder))
+				  #add the path worklist to backup where we will give the final instructions to zip in the end
   
-      if checkappdata(appdatafolder,topdirectory)==False:
-        print "the program " + progname +".pnd does not have a appdata folder so far." 
-      else:
-        
-        directoriesinappfolder=os.listdir(("/media/{0}/pandora/appdata/{1}").format(topdirectory,appdatafolder))
-        
-        for onefolder in directoriesinappfolder:
-          for foldertobackup in listfolders:
-            if onefolder==foldertobackup:
-              print "I found the folder called "+ onefolder+" to backup"
-              
-              directorytobackup.append(("/media/{0}/pandora/appdata/{1}/{2}").format(topdirectory,appdatafolder,onefolder))
-              #add the path worklist to backup where we will give the final instructions to zip in the end
-              
-  
-        #instruction to backup goes here
-  
-  makearchivefile()
-  
-  pass
-  
+			#need to build functions for files as well
+			
+#builds the archive files
 def makearchivefile():
   
   global directorytobackup, directories, today, shutil
   
   pathtoarchive=""
+  print directories
 
   for topdirectory in directories:
   
-    if pathtoarchive=="":
-      archivename="BGSfile{0}-{1}-{2}.tar.gz".format(today.year,today.month,today.day)
-      if os.path.isfile("/media/{0}/{1}".format(topdirectory,archivename))==True :
-        print "Careful, an archive with the same name already exists.
-      pathtoarchive="/media/{0}/{1}".format(topdirectory,archivename)
-      tar = tarfile.open(pathtoarchive, "w:gz")
-      for folder in directorytobackup:
-        tar.add(folder)
-      tar.close()
-    
-    else: 
-      
-      shutil.copy2(pathtoarchive, '/media/{0}'.format(topdirectory)))
-      print "Copy completed on the volume "+topdirectory
+	  if pathtoarchive=="":
+		  archivename="BGSfile{0}-{1}-{2}.tar.gz".format(today.year,today.month,today.day)
+		  if os.path.isfile("/media/{0}/{1}".format(topdirectory,archivename))==True :
+			print "Careful, an archive with the same name already exists."
+		  pathtoarchive="/media/{0}/{1}".format(topdirectory,archivename)
+		  
+		  tar = tarfile.open(pathtoarchive, "w:gz")
+		  for folder in directorytobackup:
+			tar.add(folder)
+			print "added "+folder
+		  tar.close()
+		  sizeofarchive=int((os.path.getsize(pathtoarchive))/(1024*1024))
+		  a=PyZenity.InfoMessage('Backup has been completed as {0} in /media/{1}/. The file size is {2} Mb.It will now be duplicated on the other SD card (if you have another one inserted)'.format(archivename,topdirectory,sizeofarchive),timeout=15)
+		
+	  else: 
+		if len(directories)>1:
+			
+			print pathtoarchive
+			shutil.copyfile(pathtoarchive,'/media/{0}/{1}'.format(topdirectory,archivename))
+			print "Copy completed on the volume "+topdirectory
+			a=PyZenity.InfoMessage('For redundancy purpose, {0} has also been copied to /media/{1}/'.format(archivename,topdirectory),timeout=3)
 
-
-#each program has
-#PND attached to it (single) - single variable
-#appdata attached to it (single) - single variable
-#folders to save (single or several) - list
-#files to save (single or several) - list
-
-
+		else:
+			a=PyZenity.InfoMessage('It appears you have only one SD Card inserted at the moment, and therefore the BGS file cannot be duplicated. Be aware you are exposed to a greater risk of data loss unless you duplicate this file somewhere else.',timeout=10)
+			
+			
 def checkprogram(progname,directory):
   #returns folder if the PND is found in one of the appsfolder directory or "" if not found
   global appsfolder
@@ -93,19 +128,21 @@ def checkprogram(progname,directory):
   
   for folder in appsfolder:
     if os.path.isfile("/media/{0}/pandora/{1}/{2}".format(directory,folder,progname))==True :
-      print "Found "+ progname+ " in "+ folder
+      print "Found "+ progname+ " in "+ folder+" in "+directory
       found+=1
   if found==0:
     return ""
   else:
     return folder
 
+#checks if the related appfolder exists
 def checkappdata(appdatafolder,directory):
-  if os.path.isdir("/media/{0}/pandora/appdata/{1]".format(directory,appdatafolder))==True:
+  if os.path.isdir("/media/{0}/pandora/appdata/{1}".format(directory,appdatafolder))==True:
     return True
   else:
     return False
 
+#checks if a certain folder exists
 def checkfolderexists(path,foldername):
   if os.path.isdir(path+foldername):
     return True
@@ -113,59 +150,74 @@ def checkfolderexists(path,foldername):
     return False
 
 def defineglobaldirectories():
-  #define the top levels directories in /media
+  #define the top levels directories in /media and writes a global variable
   global directories, debug
   directories=os.listdir("/media")
+  directories.remove("hdd")
+  directories.remove("ram")
+  
+  for directory in directories:
+	  if directory[0]==".":
+		  directories.remove(directory)
+  
   if debug==True: 
     print directories
 
+#finds previous BGS files if they exist and ask to erase or not
+def findpreviousbgs():
+	global directories
+	result=[]
+	for topdirectory in directories:
+		caca=glob.glob('/media/{0}/BGS*'.format(topdirectory))
+		if caca!=[]:
+			for element in caca:
+				
+				result.append(element)
+	
+	print result
+	
+	if result!=[]:
+		resultstring=""
+		sizetotal=0
+		for element in result:
+			resultstring+=element+" "
+			sizetotal+=int((os.path.getsize(element))/(1024*1024))
+		a=PyZenity.Question("I found previous BGS files: {0}.They are taking a total of {1} Mb in size. Ok to delete them and create new ones ?".format(resultstring,str(sizetotal)))
+		if a==True:
+			for element in result:
+				os.remove(element)
+			a=PyZenity.InfoMessage("Previous BGS files were deleted")
+		else:
+			a=PyZenity.InfoMessage("Previous BGS files were kept")					
+
+#displays the list of programs found to be backed up for save data
+def displayprogstobackup():
+  global programsfound
   
+  programsfoundstring=""
+  for program in programsfound:
+	  programsfoundstring+=program + " "
+	  
+  a=PyZenity.InfoMessage('Found the following to backup: {0}'.format(programsfoundstring),timeout=10)
+  a=PyZenity.InfoMessage('Backup process will now start. Note that it may take a while if you have tons of large files and saves. Do not turn off your Pandora until completion',timeout=10)
+
+#main program
 def bgs():
-#check for Pandora folder
   
   global listoftemplates
-  
+  a=PyZenity.InfoMessage('Welcome to BGS. This tool will backup your saved games from numerous emulators or native games, and save them in a single archive. That archive will then be saved on your SD Card and duplicated on your second SD card if you have any inserted, to fight the risk of data loss by redundancy.',timeout=15)
   defineglobaldirectories()
-  #get the name of global directories
   
+  findpreviousbgs()
+    
   for programtobackup in listoftemplates:
     backupspecific(programtobackup[0],programtobackup[1],programtobackup[2],programtobackup[3])
-    
   
-
-            
-#            for i=2 to len(progs):
-#              if confirmfolderorfile(progs[i])==True:
-#                folderstring=progs[i]
-#                path="/media/{0}/pandora/appdata/{1}/".format(directory,progs[1])
-#                if checkfolderexists(path, folderstring[7:])==True:
-                  #if not empty save it in list of what to save later
-#                else: 
-#                  pass
-#              else:
-                #find the files with the extension
-                #save their full path location for later saving
-          
-    # give message regarding how many files are being saved, and how big they are uncompressed
-    #save first zip for first SD CARD - add date in name
-    #confirm size after compression
-
-
-  #return a list with media as fist entry, folder as second entry, list of files detected as third entry
+  displayprogstobackup()
+  print directorytobackup
+  makearchivefile()
   
-  
+  a=PyZenity.InfoMessage('The application will now auto-close. Thanks for using it! Remember you should have these backups in case one of your card fails!',timeout=3)
+
 
 bgs()
-
-
-#test for directories such as Pandora -> to accelerate the loops
-#os.path.isdir -> then store in config.
-#allow to refresh directories lookup in one click.
-#display what systems/games are recognized
-#test for directory for emulators and all / and know where they save their saved games – for the ones where path is fixed.
-#for the ones where individual folders need to add option to add appropriate folders and save them in config file. 
-#list all files to be saved, indicate total uncompressed size in status bar as well as number of save files.
-#propose saving compressed data to media (both SD cards) / Cloud / ftp
-#compress files
-#copy files in the right place
-#when executed, register when the last backup was made, as well as the list of what was backed up.
